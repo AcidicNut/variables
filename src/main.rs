e01c4f18bba16ed317a340b2e1e09566cc76fe98
acidicnut <lorinczb96@gmail.com>
2022-02-12 13:01:43 +0100

    fibo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut n = String::new();
+    println!("num here");
+
+    io::stdin().read_line(&mut n).expect("Ohh no");
+
+    let n: u32 = n.trim().parse().expect("nut num");
+
+    let mut first = 0;
+    let mut second = 1;
+    let mut i = 1;
+
+    let result = loop {
+        if i == n {
+            break second;
+        }
+        i += 1;
+        let temp = first;
+        first = second;
+        second += temp;
+    };
+
+    println!("result: {}", result);
 }
